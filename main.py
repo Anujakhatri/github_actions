ecc57ab5ea5728da077f21b2a1dfd77d3c0031a7
Anuja khatri <khatrianuja20@gmail.com>
2026-06-07 15:44:46 +0545

    Fix: merge all steps into one block

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hello GitHub Actions")
+num1=int(input("Enter a number: "))
+num2=int(input("Enter another number: "))
+sum=num1+num2
+print("The sum is",sum)
+
+for num in range(1,11):
+    print(num)
